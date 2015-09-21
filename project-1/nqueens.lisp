11e1de8ad3218731b046aef7684e5e55e377b759
SAM <samvbeckmann@gmail.com>
2015-09-20 21:56:51 -0500

    Fixed NQueens and Permutations.

--- project-1/nqueens.lisp
+++ project-1/nqueens.lisp
@@ -1,28 +1,74 @@
-; returns the row in the given column that has the least number of conflicts with the current board state. TODO: Testing
-(defun findMinConflicts (board col, endRow, startRow)
+; returns the row in the given column that has the least number of conflicts with the current board state.
+(defun find-min-conflicts (board col endRow &optional (startRow 0))
   (if (>= startRow endRow)
       endRow
-	  (if (< (checkConflicts board (newPair col startRow)) (checkConflicts board col endRow (findBig board col endRow (1+ startRow))))
-	      startRow
-		  (findBig board col endRow (1+ startRow)))))
-		  
-; checks to see if the board is solved in its current state TODO: Testing
-(defun checkSolved (board n &optional (col 1))
-  (cond ((>= col n)
-            T)
-		((equal 0 (checkConflicts board (getQueenInCol board col)))
-		    (checkSolved board n (1+ col)))
-	    (t nil)))
-		
-; needed functions
-; - getQueenInCol (board col): returns of pair that contains the queen in the given column, if one exists.
-; - checkConflicts(board position): returns the number of conflicts a queen in a given position has
-; - nqueens(n): Runs the nqueens program
-; - displayResults(board): Prints the state of a completed board to the console.
-; - assignBoard(n): creates a new board from scratch
+	  (let ((min-rest (find-min-conflicts board col endRow (1+ startRow))))
+	       (if (< (check-conflicts board (list col startRow)) (check-conflicts board (list col min-rest)))
+	        startRow
+		    min-rest))))
 
-(defun nqueens(n )
-  )
+; returns the pair of the queen in the given column, if one exists
+(defun get-queen-in-col (board col)
+  (cond ((<= (length board) 0) nil)
+        ((equal (first (first board)) col) (first board))
+		(t (get-queen-in-col (rest board) col))))
+
+; returns the number of conflicts a position has on the board
+(defun check-conflicts (board position &aux(counter 0))
+  (dolist (queen board counter)
+    (if (is-conflict queen position) (incf counter) ())))
+
+; tests if there is a conflict between two positions
+(defun is-conflict (pos1 pos2)
+  (cond ((equal pos1 pos2) nil)
+        ((= (first pos1) (first pos2)) t)
+        ((= (second pos1) (second pos2)) t)
+		((= (abs (- (first pos1) (first pos2))) (abs (- (second pos1) (second pos2)))) t)
+		(t nil)))
+
+; finds the queen on the board that currently has the max # of conflicts
+(defun find-max-conflicts (board endcol &optional (startcol 0))
+  (if (>= startcol endcol) endcol
+      (if (> (check-conflicts board (get-queen-in-col board startcol)) (check-conflicts board (get-queen-in-col board (find-max-conflicts board endcol (1+ startcol)))))
+	      startcol
+		  (find-max-conflicts board endcol (incf startcol)))))
+
+; gets the col of a queen from the set of conflicted queens.
+(defun get-conflicted-queen (board)
+  (loop
+    (let ((col (random (length board))))
+	  (if (> (check-conflicts board (get-queen-in-col board col)) 0)
+	      (return col)))))
   
-(defun getQueenInCol (board col)
-  )
+
+; removes the current queen in the given col, replacing it with one in the newrow arg.		
+(defun update-queen (board col newrow)
+  (dolist (queen board)
+    (if (= (first queen) col)
+	    (return (cons (list col newrow) (remove queen board))))))
+		
+; makes the initial random assignments for the board		
+(defun assign-board (n &optional (x 0)(board ()))
+  (if (>= x n) board
+      (assign-board n (1+ x) (cons (list x (random n)) board))))
+
+; checks to see if the board is solved in its current state  
+(defun no-conflicts (board &aux (conflicts 0))
+  (dolist (queen board conflicts)
+    (if (not (= (check-conflicts board queen) 0))
+	    (incf conflicts))))
+
+; performs a single step of the nqueens program.
+(defun unit-step (board n steps-remain)
+  (cond ((<= steps-remain 0) nil)
+        ((= 0 (no-conflicts board)) board)
+		(t (let ((col (get-conflicted-queen board)))
+		    (unit-step (update-queen board col (find-min-conflicts (remove (get-queen-in-col board col) board) col (1- n))) n (decf steps-remain))))))
+
+; fucntion that drives the nqueens program
+(defun nqueens(n maxsteps)
+  (unit-step (assign-board n) n maxsteps))
+  
+; Note on output:
+; an output of nil means no solutions was found.
+; an output of of a list is the result. The first item in each pair is the column of a queen, the next number is a row. The list is zero-indexed.
--- project-1/permutations.lisp
+++ project-1/permutations.lisp
@@ -1,6 +1,6 @@
+; Returns all the permutations of a given list.
 (defun permutations (arg &aux(result ()))
-  (if (equal (length arg) 1) arg
+  (if (equal (length arg) 1) (list arg)
       (dolist (item arg result)
 	    (dolist (val (permutations (remove item arg)))
-		  (append result (cons item val)))
-		(write result))))
+		  (push (cons item val) result)))))
